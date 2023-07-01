f4712c0eb1bc603ebdc6197c213ef90b4bcee07c
richard <liu22291@spengergasse.at>
2023-07-01 18:17:05 +0200

    Model classes

---
+++ TaskTrackerProject/TaskTrackerProject.Application/Model/User.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Principal;
+
+namespace TaskTrackerProject.Application.Model
+{
+    [Index(nameof(Email), IsUnique = true)]
+    [Index(nameof(Username), IsUnique = true)]
+    public class User
+    {
+        public User(string username, string password, string email, Userrole role)
+        {
+            Username = username;
+            Password = password;
+            Email = email;
+            Role = role;
+        }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; private set; }
+        public Guid Guid { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public Userrole Role { get; set; }
+    }
+}
