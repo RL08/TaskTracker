d91057b9761f4cc11c0ccc1047e7f6432b1a526f
richard <liu22291@spengergasse.at>
2023-07-13 14:21:27 +0200

    git

---
+++ startServer.cmd
@@ -0,0 +1,11 @@
+@echo off
+docker rm -f TaskTracker_sqlserver 2> nul
+docker run -d -p 11433:1433 --name TaskTracker_sqlserver -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=SqlServer2019" mcr.microsoft.com/azure-sql-edge
+IF ERRORLEVEL 1 GOTO error
+dotnet build TaskTrackerProject\TaskTrackerProject.Webapi --no-incremental --force
+dotnet watch run -c Debug --project TaskTrackerProject\TaskTrackerProject.Webapi
+GOTO end
+
+:error
+PAUSE
+:end
